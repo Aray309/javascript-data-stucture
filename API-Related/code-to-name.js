0bc434c20823fa63e0310dc818ab8946294c6614
Anurag K Ray <anuragannu309@gmail.com>
2024-01-18 10:10:19 +0530

    New Changes in file

--- API-Related/code-to-name.js
+++ API-Related/code-to-name.js
@@ -15,14 +15,26 @@ const codeToNameGenertor=(input)=>{
     if(current){
         result.push(Number(current))
     }
-    const formatData = result.map((element)=>{
-        if(!isNaN(element)){
-            return element;
-        }else if(element==="D"){
-            return "Days"
+    const mapping= {
+        "A": "Actual date",
+        "B": "Business day",
+        "D": "Day",
+        "E": "End of month",
+        "M": "Month",
+        "N": "Next business date",
+        "P": "Previous business date",
+        "W": "Week",
+        "Y": "Year"
+      };
+    
+      const formatData = result.map((element) => {
+        if (typeof element === 'string') {
+          const key = element.toUpperCase();
+          return mapping[key];
         }
-    });
-    return formatData
+        return element;
+      });
+      return formatData;
 }
 const formatData = codeToNameGenertor("1D33")
 console.log(formatData);
